21be17ff889a247dcd8ab9546226680d49402498
Chaithra-KR <chaithrakr549@gmail.com>
2023-07-25 15:02:37 +0530

    initial commit

---
+++ src/Components/Create/Create.js
@@ -0,0 +1,95 @@
+import React, { Fragment, useContext, useState } from 'react';
+import './Create.css';
+import Header from '../Header/Header';
+import { AuthContext, firebaseContext } from '../../store/firebaseContext';
+import { useNavigate } from 'react-router-dom';
+
+const Create = () => {
+  const [product,setProduct] = useState('');
+  const [category,setCategory] = useState('');
+  const [price,setPrice] = useState('');
+  const [image,setImage] = useState('');
+
+  const {firebase} = useContext(firebaseContext)
+  const {user} = useContext(AuthContext)
+
+  const navigate = useNavigate()
+
+  const date = new Date()
+
+  const handleSubmit = () =>{
+    firebase.storage().ref(`/image/${image.product}`).put(image).then(({ref})=>{
+      ref.getDownloadURL().then((url)=>{
+        console.log(url);
+        firebase.firestore().collection('products').add({
+          product,
+          category,
+          price,
+          url,
+          userId :user.uid,
+          createdAt :date.toDateString()
+        })
+        navigate('/')
+      })
+    })
+  }
+
+  return (
+    <Fragment>
+      <Header />
+      <card>
+        <div className="centerDiv">
+            <label htmlFor="fname">Product :</label>
+            <input
+              className="input"
+              type="text"
+              id="fname"
+              name="Name"
+              value={product}
+              onChange={(e)=>{
+                setProduct(e.target.value)
+              }}
+            />
+            <br />
+            <label htmlFor="fname">Category :</label>
+            <input
+              className="input"
+              type="text"
+              id="fname"
+              name="category"
+              value={category}
+              onChange={(e)=>{
+                setCategory(e.target.value)
+              }}
+            />
+            <br />
+            <label htmlFor="fname">Price :</label>
+            <br />
+            <input 
+            className="input" 
+            type="number" id="fname" 
+            name="Price"
+            value={price}
+            onChange={(e)=>{
+              setPrice(e.target.value)
+            }} 
+            />
+            <br />
+          <br />
+          <img alt="Posts" width="200px" height="200px" src={image ? URL.createObjectURL(image) : ''}></img>
+            <br />
+            <input 
+            type="file" 
+            onChange={(e)=>{
+              setImage(e.target.files[0])
+            }}
+            />
+            <br />
+            <button onClick={handleSubmit} className="uploadBtn">upload and Submit</button>
+        </div>
+      </card>
+    </Fragment>
+  );
+};
+
+export default Create;
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+import 'firebase/auth'
+import 'firebase/firebase'
+import 'firebase/storage'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAy5bIytr6AY4Ua5tgv-EmL70ugKVJ919w",
+    authDomain: "fir-fd53e.firebaseapp.com",
+    projectId: "fir-fd53e",
+    storageBucket: "fir-fd53e.appspot.com",
+    messagingSenderId: "385403160140",
+    appId: "1:385403160140:web:84022672420720882816e9",
+    measurementId: "G-FK35R5JMSK"
+  };
+
+export default firebase.initializeApp(firebaseConfig)
